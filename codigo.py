40d2fd468c14dd3431c93eb3e660aeabb0a7c3ae
Bakaus <matheus.bakaus.bruno@gmail.com>
2024-03-11 14:16:40 -0300

    add Readme and updates on code

--- codigo.py
+++ codigo.py
@@ -4,14 +4,10 @@ from datetime import datetime
 
 #OBTENDO HORA DO COMPUTADOR
 data_e_hora_atuais = datetime.now()
-data_e_hora_em_texto = data_e_hora_atuais.strftime("%H")
+hora = data_e_hora_atuais.strftime("%H")
 
 ##
-if int(data_e_hora_em_texto) > 7  and int(data_e_hora_em_texto) < 12:
-    print('manha')
-elif int(data_e_hora_em_texto)> 12 and int(data_e_hora_em_texto) < 17:
-    print(data_e_hora_em_texto)
-
+if int(hora) > 7  and int(hora) < 12:
     pyautogui.press("win")
     pyautogui.write('spotify')
     pyautogui.press('enter')
@@ -23,13 +19,29 @@ elif int(data_e_hora_em_texto)> 12 and int(data_e_hora_em_texto) < 17:
     pyautogui.moveTo(622,416,0.5)
     time.sleep(3)
     pyautogui.click(button='left')
+elif int(hora)> 12 and int(hora) < 17:
+
+    pyautogui.press("win")
+    pyautogui.write('spotify')
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.moveTo(98,150,0.5)
+    pyautogui.click(button='left')
+    time.sleep(2)
+    pyautogui.write('Tarde - Dom Bosco')
+    pyautogui.moveTo(622,416,0.5)
+    time.sleep(3)
+    pyautogui.click(button='left')
 else:
-    print('noite')
-
-
-
-
-#pyautogui.write(link)
-#time.sleep(1)
-#pyautogui.press('enter')
+    pyautogui.press("win")
+    pyautogui.write('spotify')
+    pyautogui.press('enter')
+    time.sleep(3)
+    pyautogui.moveTo(98,150,0.5)
+    pyautogui.click(button='left')
+    time.sleep(2)
+    pyautogui.write('UNIFATEB')
+    pyautogui.moveTo(622,416,0.5)
+    time.sleep(3)
+    pyautogui.click(button='left')
 
