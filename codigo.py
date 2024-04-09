026568c1a0fbfb5907f5cc7b5d9d7a6eab307f0c
Bakaus <matheus.bakaus.bruno@gmail.com>
2024-04-09 13:47:31 -0300

    ok

--- codigo.py
+++ codigo.py
@@ -9,50 +9,50 @@ dia = data_e_hora_atuais.strftime("%D")
 
 #VALIDANDO QUAL HORÁRIO ESTAMOS E QUAL PLAYLIST DEVERÁ SER TOCADA
 if int(hora) >= 7  and int(hora) <= 12:
-    ##ABRINDO SPOTIFY
-    #pyautogui.moveTo(37,58)
-    #pyautogui.leftClick()
-    #time.sleep(15)
-    ##SELECIONANDO PLAYLIST
-    #pyautogui.moveTo(113,511)
-    #pyautogui.leftClick()
-    #time.sleep(5)
-    ##CLICANDO NO PLAY
-    #pyautogui.moveTo(210,442)
-    #pyautogui.leftClick()
+    #ABRINDO SPOTIFY
+    pyautogui.moveTo(37,58)
+    pyautogui.leftClick()
+    time.sleep(15)
+    #SELECIONANDO PLAYLIST
+    pyautogui.moveTo(113,511)
+    pyautogui.leftClick()
+    time.sleep(5)
+    #CLICANDO NO PLAY
+    pyautogui.moveTo(210,442)
+    pyautogui.leftClick()
     with open("log.txt", "a") as file1:
         file1.write("\n OK Manha - " + data_e_hora_atuais.strftime("%H:%M") + " " + dia )
     
 
 elif int(hora)> 12 and int(hora) < 17:
-    ##ABRINDO SPOTIFY
-    #pyautogui.moveTo(37,58)
-    #pyautogui.leftClick()
-    #time.sleep(15)
-    ##SELECIONANDO PLAYLIST
-    #pyautogui.moveTo(109,438)
-    #pyautogui.leftClick()
-    #time.sleep(5)
-    ##CLICANDO NO PLAY
-    #pyautogui.moveTo(210,442)
-    #pyautogui.leftClick()
+    #ABRINDO SPOTIFY
+    pyautogui.moveTo(37,58)
+    pyautogui.leftClick()
+    time.sleep(15)
+    #SELECIONANDO PLAYLIST
+    pyautogui.moveTo(109,438)
+    pyautogui.leftClick()
+    time.sleep(5)
+    #CLICANDO NO PLAY
+    pyautogui.moveTo(210,442)
+    pyautogui.leftClick()
     with open("log.txt", "a") as file1:
         file1.write("\n OK Tarde - " + data_e_hora_atuais.strftime("%H:%M") + " " + dia  )
     
 
 
 else:
-    ##ABRINDO SPOTIFY
-    #pyautogui.moveTo(37,58)
-    #pyautogui.leftClick()
-    #time.sleep(15)
-    ##ESCOLHENDO PLAYLIST
-    #pyautogui.moveTo(108,381)
-    #pyautogui.leftClick()
-    #time.sleep(5)
-    ##CLICANDO NO PLAY
-    #pyautogui.moveTo(210,442)
-    #pyautogui.leftClick()
+    #ABRINDO SPOTIFY
+    pyautogui.moveTo(37,58)
+    pyautogui.leftClick()
+    time.sleep(15)
+    #ESCOLHENDO PLAYLIST
+    pyautogui.moveTo(108,381)
+    pyautogui.leftClick()
+    time.sleep(5)
+    #CLICANDO NO PLAY
+    pyautogui.moveTo(210,442)
+    pyautogui.leftClick()
     with open("log.txt", "a") as file1:
         file1.write("\n OK Noite - " + data_e_hora_atuais.strftime("%H:%M") + " " + dia )
     
